bda50e7a1fa499174bec3f706b1729bb880aacb5
Naomi Ezaki <naomi.ezaki28@gmail.com>
2020-01-20 14:15:04 +0800

    added new files

--- app/User.php
+++ app/User.php
@@ -28,6 +28,13 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+
+    public function address(){
+        return $this->hasOne('App\Address');
+    }
+
+
+
     /**
      * The attributes that should be cast to native types.
      *
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Address;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +17,58 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+/*
+    ONE TO ONE CRUD
+
+    //Step 1:
+        php artisan make:model Address -m
+
+    //Step 2: Add code to database/migrations/2020_01_20_052023_create_addresses_table.php:
+        $table->integer('user_id')->unsigned()->nullable();
+        $table->string('name');
+
+    //Step 3:
+        php artisan migrate
+
+    //Step 4: Add code to app/User.php:
+        public function address(){
+            return $this->hasOne('App\Address');
+        }
+*/
+    //Route:
+    //I. Creating Data:
+            Route::get('/insert', function(){
+                $user = User::findOrFail(1);
+
+                $address = new Address(['name'=>'4435 Paulina av NY NY 11218']);
+
+                $user->address()->save($address);
+            });
+    
+    //II. Updating Data:
+            Route::get('/update', function(){
+                $address = Address::whereUserId(1)->first();
+                $address->name = "4353 Update Av, Alaska";
+
+                $address->save();
+            });
+    
+    //III. Reading Data
+            Route::get('/read', function(){
+                $user = User::findOrFail(1);
+
+                echo $user->address->name;
+            });
+    
+    //IV. Deleting Data
+            Route::get('/delete', function(){
+                $user = User::findOrFail(1);
+
+                $user->address()->delete();
+            });
+
+
+
